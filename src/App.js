b7db737b74a7d63d13484acb0b02f799153d2ea5
nrico90 <nrico90@gmail.com>
2019-12-11 14:13:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+import store from "./store";
+import { Provider } from "react-redux";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Route path="/" exact component={Home} />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/actions/advertisements.js
@@ -0,0 +1,27 @@
+import request from "superagent";
+
+export const ADVERTISEMENTS_FETCHED = "ADVERTISEMENTS_FETCHED";
+export const ADVERTISEMENT_FETCHED = "ADVERTISEMENT_FETCHED";
+export const NEW_ADVERTISEMENT = "NEW_ADVERTISEMENT";
+export const DELETE_ADVERTISEMENT_SUCCES = "DELETE_ADVERTISEMENT_SUCCES";
+export const ADVERTISEMENT_UPDATE_SUCCESS = "ADVERTISEMENT_UPDATE_SUCCESS";
+
+const baseUrl = "http://localhost:4000";
+
+const advertisementsFetched = advertisements => ({
+  type: ADVERTISEMENTS_FETCHED,
+  payload: advertisements
+});
+
+export const loadAdvertisements = () => (dispatch, getState) => {
+  // when the state already contains events, we don't fetch them again
+  if (getState().advertisements.length) return;
+
+  // a GET /events request / function in a function, thunk
+  request(`${baseUrl}/advertisements`)
+    .then(response => {
+      // dispatch an EVENTS_FETCHED action that contains the events
+      dispatch(advertisementsFetched(response.body));
+    })
+    .catch(console.error);
+};
